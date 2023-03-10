afe6caebe91b815e58ffae8029c9bf088b709d74
Gbubemi Ogbe <ogbegbubemi15@gmail.com>
2023-03-10 08:17:59 +0100

    Redo as JSX. TSX throwing up errors

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,17 +1,15 @@
 import Image from 'next/image';
 
-export async function getStaticProps() {
+export async function generateStaticParams() {
 	const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
 	const res = await data.json();
 
-	return {
-		props: {
-			res,
-		},
-	};
+	return res.results.map((movie) => ({
+		movie: toString(movie.id),
+	}));
 }
 
-export default async function MovieDetail({ params }: { params: paramsType }) {
+export default async function MovieDetail({ params }) {
 	const { movie } = params;
 
 	const imagePath = 'https://image.tmdb.org/t/p/original';
@@ -30,25 +28,4 @@ export default async function MovieDetail({ params }: { params: paramsType }) {
 			</div>
 		</div>
 	);
-}
-
-type paramsType = {
-	movie: string;
-};
-
-interface movieInterface {
-	adult?: boolean;
-	backdrop_path?: string;
-	genre_ids?: [Array<number>];
-	id?: number;
-	original_language?: string;
-	original_title?: string;
-	overview?: string;
-	popularity?: string;
-	poster_path?: string;
-	release_date?: string;
-	title?: string;
-	video?: boolean;
-	vote_average?: number;
-	vote_count?: number;
 }
